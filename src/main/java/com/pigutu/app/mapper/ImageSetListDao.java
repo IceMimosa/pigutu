86b81ac330a64feaa60427a39a4e5d88bb69ac24
xuxinwei <xuxinwei@shandagames.com>
2017-09-11 21:22:04 +0800

    上传图片方法2

--- src/main/java/com/pigutu/app/mapper/ImageSetListDao.java
+++ src/main/java/com/pigutu/app/mapper/ImageSetListDao.java
@@ -182,4 +182,9 @@ public interface ImageSetListDao {
             @Result(property = "create_time", column = "create_time")
     })
     List<ImageSetListEntity> myRecommend(@Param("page") int pageStart,@Param("pageNumber")int pageNumber);
+
+    //@Param("id")int id,@Param("title")int title,@Param("cover_url")int cover_url,@Param("like_count")int like_count,@Param("img_count")int img_count,@Param("comment_count")int comment_count,
+    // @Param("category")int category,@Param("label")int label,@Param("create_time")int create_time,@Param("view_count")int view_count,@Param("recommend_count")int recommend_count
+    @Insert({"insert into image_set_list values(#{id},#{title},#{cover_url},#{like_count},#{img_count},#{comment_count},#{category},#{label},#{create_time},#{view_count},#{recommend_count})"})
+    int insertImageSetList(ImageSetListEntity imageSetListEntity);
 }
