554c43a2e9f99464ef40ac8ff863420918a9bf72
eric <me67554@gmail.com>
2018-07-02 00:37:21 +0800

    定时任务改成12点28分触发,fix comment bug

--- src/main/java/com/pigutu/app/test/Jitaotu.java
+++ src/main/java/com/pigutu/app/test/Jitaotu.java
@@ -1,22 +1,7 @@
 package com.pigutu.app.test;
 
-import com.pigutu.app.utils.AESUtil;
-import com.pigutu.app.utils.GsonHelper;
-import com.pigutu.app.utils.JitaotuHelper;
-import com.pigutu.app.utils.OssHelper;
-import com.sun.org.apache.xerces.internal.impl.dv.util.Base64;
-import org.jsoup.Jsoup;
-import org.jsoup.nodes.Document;
-import org.jsoup.nodes.Element;
-import org.jsoup.select.Elements;
-
-import java.io.IOException;
-import java.io.UnsupportedEncodingException;
-import java.util.ArrayList;
-
 public class Jitaotu {
     public static void main(String args[]) {
-        DA db = new DB();
-        System.out.println(db.a);
+
     }
 }
