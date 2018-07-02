4f30365be50b67f87e2253efd1112d024d8c47b6
eric <me67554@gmail.com>
2018-07-03 01:14:57 +0800

    config加密，如果是2，下面接口不加密，不然全加密

--- src/main/java/com/pigutu/app/entity/ResponseReturn.java
+++ src/main/java/com/pigutu/app/entity/ResponseReturn.java
@@ -1,5 +1,6 @@
 package com.pigutu.app.entity;
 
+import com.pigutu.app.config.GlobalConfig;
 import com.pigutu.app.exception.ErrorCode;
 import com.pigutu.app.utils.AESUtil;
 import com.pigutu.app.utils.GsonHelper;
@@ -18,21 +19,34 @@ public class ResponseReturn {
     private String msg;
     private Object data;
 
-    public ResponseReturn(int code, String msg, Object data){
-        this.code=code;
-        this.msg=msg;
+    public ResponseReturn(int code, String msg, Object data) {
+        this.code = code;
+        this.msg = msg;
         this.data = data;
     }
 
+    //0 正常，1加密+forchannel，2不加密+forchannel
     public static ResponseReturn success(Object data) {
       /*  String base64Str = Base64.encode("p112g".getBytes());
         base64Str = base64Str+base64Str;
         log.debug(base64Str);*/
-       // return new ResponseReturn(0,"success", AESUtil.encrypt(GsonHelper.INSTANCE.getGson().toJson(data), base64Str));
-        return new ResponseReturn(0,"success", data);
+        // return new ResponseReturn(0,"success", AESUtil.encrypt(GsonHelper.INSTANCE.getGson().toJson(data), base64Str));
+        if (!GlobalConfig.INSTANCE.getChannelValue().equals("2")) {
+            return successBase64(data);
+        } else {
+            return new ResponseReturn(0, "success", data);
+        }
+    }
+
+    public static ResponseReturn successBase64(Object data) {
+        String base64Str = Base64.encode("p112g".getBytes());
+        base64Str = base64Str + base64Str;
+        log.debug(base64Str);
+        // return new ResponseReturn(0,"success", AESUtil.encrypt(GsonHelper.INSTANCE.getGson().toJson(data), base64Str));
+        return new ResponseReturn(0, "success", AESUtil.encrypt(GsonHelper.INSTANCE.getGson().toJson(data), base64Str));
     }
 
     public static ResponseReturn error(int code) {
-        return new ResponseReturn(code, ErrorCode.getMsg(code),null);
+        return new ResponseReturn(code, ErrorCode.getMsg(code), null);
     }
 }
